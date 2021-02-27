2b9f5b3f3e58bc106356dd824319c0d98f80357e
sigit suprayogi <sigitsuprayogi@ymail.com>
2021-02-27 20:45:52 +0700

    Delete recaptcha.rb

--- config/initializers/recaptcha.rb
+++
@@ -1,7 +0,0 @@
-# config/initializers/recaptcha.rb
-Recaptcha.configure do |config|
-  config.site_key   = '6Lc4oZ8UAAAAADZG_Dt6NkiPag5nMJlpTrq7IVkZ'
-  config.secret_key = '6Lc4oZ8UAAAAABgoneCd8_hqYSJN5S4_8qbR-QOm'
-  # Uncomment the following line if you are using a proxy server:
-  config.proxy = 'http://127.0.0.1:3000'
-end	
